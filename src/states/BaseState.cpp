00a3969fefcd837cdb55d9896c1be1a0d9ce8bee
Filmote <simon@bloggingadeadhorse.com>
2018-10-06 21:42:02 +1000

    Allw 5 digit score.

--- src/states/BaseState.cpp
+++ src/states/BaseState.cpp
@@ -11,14 +11,32 @@ void BaseState::renderScore(StateMachine & machine, int16_t score, uint8_t x, ui
 
 	auto & arduboy = machine.getContext().arduboy;
 
-	uint8_t digits[4] = {};
-	extractDigits(digits, static_cast<uint16_t>(absT(score)));
-
 	if (score < 0) { arduboy.drawFastHLine(x - 3, y + 3, 2); }
 
-	for (uint8_t j = 4, x2 = x; j > 0; --j, x2 += 4) {
-		font3x5.setCursor(x2, y);
-		font3x5.print(digits[j - 1]);
+	if (score > 10000) {
+
+		for (uint8_t j = 5, x2 = x - 4; j > 0; --j, x2 += 4) {
+			
+			uint8_t digits[5] = {};
+			extractDigits(digits, static_cast<uint16_t>(absT(score)));
+
+			font3x5.setCursor(x2, y);
+			font3x5.print(digits[j - 1]);
+
+		}
+
+	}
+	else {
+
+		for (uint8_t j = 4, x2 = x; j > 0; --j, x2 += 4) {
+			
+			uint8_t digits[4] = {};
+			extractDigits(digits, static_cast<uint16_t>(absT(score)));
+
+			font3x5.setCursor(x2, y);
+			font3x5.print(digits[j - 1]);
+		}
+
 	}
 
 }
